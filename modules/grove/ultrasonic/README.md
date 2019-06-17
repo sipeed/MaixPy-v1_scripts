dba1bd229d8c4d011da82ad0520ffa7038557df9
Neucrack <czd666666@gmail.com>
2019-06-17 16:56:21 +0800

    addadd Grove ultrasonic module

---
+++ modules/grove/ultrasonic/README.md
@@ -0,0 +1,14 @@
+Grove Ultrasonic Ranger
+=======
+
+<img src="assets/ultrasonic.jpg" width=450 /> <img src="assets/ultrasonic2.jpg" width=450 />
+
+
+More info see [Seeed Studio Wiki](http://wiki.seeedstudio.com/Grove-Ultrasonic_Ranger/)
+
+## Examples
+
+* [measure](./measure.py)
+
+
+
---
+++ modules/grove/ultrasonic/measure.py
@@ -0,0 +1,17 @@
+
+from fpioa_manager import *
+from modules import ultrasonic
+import time
+
+
+fm.register(board_info.D[6], fm.fpioa.GPIOHS0)
+
+device = ultrasonic(fm.fpioa.GPIOHS0)
+
+while True:
+    distance = device.measure(unit = ultrasonic.UNIT_CM, timeout = 3000000)
+    print(distance)
+    time.sleep_ms(100)
+
+
+
