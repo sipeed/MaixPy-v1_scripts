6928fe172a77402915850a27c6f8fe04a75b8223
Neucrack <czd666666@gmail.com>
2019-07-05 11:42:05 +0800

    update adc read demo

--- network/demo_esp32_read_adc.py
+++ network/demo_esp32_read_adc.py
@@ -1,23 +1,23 @@
+
 import nes,network
 import utime
 from Maix import GPIO
 from fpioa_manager import *
 
 #iomap at MaixDuino
-fm.register(25,fm.fpioa.GPIOHS10, force=True)#cs
-fm.register(8,fm.fpioa.GPIOHS11, force=True)#rst
-fm.register(9,fm.fpioa.GPIOHS12, force=True)#rdy
-fm.register(28,fm.fpioa.GPIOHS13, force=True)#mosi
-fm.register(26,fm.fpioa.GPIOHS14, force=True)#miso
-fm.register(27,fm.fpioa.GPIOHS15, force=True)#sclk
+fm.register(25,fm.fpioa.GPIOHS10)#cs
+fm.register(8,fm.fpioa.GPIOHS11)#rst
+fm.register(9,fm.fpioa.GPIOHS12)#rdy
+fm.register(28,fm.fpioa.GPIOHS13)#mosi
+fm.register(26,fm.fpioa.GPIOHS14)#miso
+fm.register(27,fm.fpioa.GPIOHS15)#sclk
 
 nic = network.ESP32_SPI(cs=fm.fpioa.GPIOHS10,rst=fm.fpioa.GPIOHS11,rdy=fm.fpioa.GPIOHS12,
 mosi=fm.fpioa.GPIOHS13,miso=fm.fpioa.GPIOHS14,sclk=fm.fpioa.GPIOHS15)
 
-adc_pin = ["PIN36", "PIN39", "PIN34", "PIN35", "PIN32"]
-
 while True:
     adc = nic.adc()
-    for i in range(len(adc)):
-        print("%s:%d"%(adc_pin[i],adc[i]))
+    for v in adc:
+        print("%04d" %(v), end=" ")
+    print()
     utime.sleep_ms(50)
