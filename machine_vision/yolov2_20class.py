54811dbed403d13b77cfe08b2251987e2d8d78e4
Caize Wu <zepanwucai@gmail.com>
2019-10-27 11:57:46 +0800

    Update yolov2_20class.py

--- machine_vision/yolov2_20class.py
+++ machine_vision/yolov2_20class.py
@@ -1,21 +1,30 @@
-#refer to http://blog.sipeed.com/p/675.html
-import sensor,image,lcd
+#refer to http://blog.sipeed.com/p/677.html
+import sensor,image,lcd,time
 import KPU as kpu
-lcd.init()
+
+lcd.init(freq=15000000)
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
-sensor.set_hmirror(0)
+sensor.set_vflip(1)
 sensor.run(1)
-task = kpu.load(0x300000) 
-anchor = (1.889, 2.5245, 2.9465, 3.94056, 3.99987, 5.3658, 5.155437, 6.92275, 6.718375, 9.01025)
+clock = time.clock()
+classes = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+task = kpu.load(0x500000) 
+anchor = (1.08, 1.19, 3.42, 4.41, 6.63, 11.38, 9.42, 5.11, 16.62, 10.52)
 a = kpu.init_yolo2(task, 0.5, 0.3, 5, anchor)
-img_lcd=image.Image()
 while(True):
+    clock.tick()
     img = sensor.snapshot()
     code = kpu.run_yolo2(task, img)
+    print(clock.fps())
     if code:
         for i in code:
-            a = img.draw_rectangle(i.rect())
-    a = lcd.display(img)
+            a=img.draw_rectangle(i.rect())
+            a = lcd.display(img)
+            for i in code:
+                lcd.draw_string(i.x(), i.y(), classes[i.classid()], lcd.RED, lcd.WHITE)
+                lcd.draw_string(i.x(), i.y()+12, '%f1.3'%i.value(), lcd.RED, lcd.WHITE)
+    else:   
+        a = lcd.display(img)
 a = kpu.deinit(task)
