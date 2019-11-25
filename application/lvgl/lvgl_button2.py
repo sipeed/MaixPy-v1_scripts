a88e282000e1686d9fa45531031869273b5d6c52
Neucrack <CZD666666@gmail.com>
2019-11-25 17:51:32 +0800

    update lvgl demo

--- application/lvgl/lvgl_button2.py
+++ application/lvgl/lvgl_button2.py
@@ -4,11 +4,18 @@ import lcd
 import time
 from machine import Timer
 from machine import I2C
-import touchscreen as ts
+
+config_touchscreen_support = True
+board_m1n = False
    
 i2c = I2C(I2C.I2C0, freq=400000, scl=30, sda=31)
-lcd.init()
-ts.init(i2c)
+if not board_m1n:
+	lcd.init()
+else:
+	lcd.init(type=2, freq=20000000)
+if config_touchscreen_support:
+	import touchscreen as ts
+	ts.init(i2c)
 lv.init()
 
 disp_buf1 = lv.disp_buf_t()
@@ -18,15 +25,20 @@ disp_drv = lv.disp_drv_t()
 lv.disp_drv_init(disp_drv)
 disp_drv.buffer = disp_buf1
 disp_drv.flush_cb = lv_h.flush
-disp_drv.hor_res = 320
-disp_drv.ver_res = 240
+if board_m1n:
+    disp_drv.hor_res = 240
+    disp_drv.ver_res = 240
+else:
+    disp_drv.hor_res = 320
+    disp_drv.ver_res = 240
 lv.disp_drv_register(disp_drv)
 
-indev_drv = lv.indev_drv_t()
-lv.indev_drv_init(indev_drv) 
-indev_drv.type = lv.INDEV_TYPE.POINTER
-indev_drv.read_cb = lv_h.read
-lv.indev_drv_register(indev_drv)
+if config_touchscreen_support:
+	indev_drv = lv.indev_drv_t()
+	lv.indev_drv_init(indev_drv) 
+	indev_drv.type = lv.INDEV_TYPE.POINTER
+	indev_drv.read_cb = lv_h.read
+	lv.indev_drv_register(indev_drv)
 
 # lv.log_register_print_cb(lv_h.log)
 lv.log_register_print_cb(lambda level,path,line,msg: print('%s(%d): %s' % (path, line, msg)))
