0ed0215caf444f8250a0563a03ed54fb729c77ab
QinYUN575 <QinYUN575@Gmail.com>
2020-07-13 16:32:51 +0800

    Modify mouse response events

--- tools_on_PC/network/pic_server.py
+++ tools_on_PC/network/pic_server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#coding:utf-8
+# coding:utf-8
 import socket
 import time
 import threading
@@ -7,14 +7,19 @@ import datetime
 import pygame
 from pygame.locals import QUIT, KEYDOWN, K_f, K_F11, FULLSCREEN
 
-local_ip   = ""
+local_ip = ""
 local_port = 3456
 width = 320
 height = 240
 
+# jpeg 20 fps
+# esp32 spi dma temp buffer MAX Len: 4k
+
+
 def receiveThread(conn):
-    conn.settimeout(30)
+    conn.settimeout(10)
     conn_end = False
+    pack_size = 1024*5
     while True:
         if conn_end:
             break
@@ -32,7 +37,7 @@ def receiveThread(conn):
             tmp = client_data
         while True:
             try:
-                client_data = conn.recv(2048)
+                client_data = conn.recv(4096)
             except socket.timeout:
                 client_data = None
                 conn_end = True
@@ -42,19 +47,19 @@ def receiveThread(conn):
             img += client_data
             if img[-2:] == b'\xFF\xD9':
                 break
-            if len(client_data) > 1024*30:
+            if len(client_data) > pack_size:
                 break
         print("recive end, pic len:", len(img))
-        
+
         if not img.startswith(b'\xFF\xD8') or not img.endswith(b'\xFF\xD9'):
             print("image error")
             continue
-        f = open("tmp.jpg","wb")
+        f = open("tmp.jpg", "wb")
         f.write(img)
         f.close()
         try:
             surface = pygame.image.load("tmp.jpg").convert()
-            screen.blit(surface,(0, 0))
+            screen.blit(surface, (0, 0))
             pygame.display.update()
             print("recieve ok")
         except Exception as e:
@@ -62,22 +67,35 @@ def receiveThread(conn):
     conn.close()
     print("receive thread end")
 
+
 pygame.init()
 screen = pygame.display.set_mode((width, height), 0, 32)
 pygame.display.set_caption("pic from client")
 
-
-ip_port = (local_ip,local_port)
+ip_port = (local_ip, local_port)
 sk = socket.socket()
-sk.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+sk.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sk.bind(ip_port)
 sk.listen(50)
 print("accept now,wait for client")
-while True:
-    conn,addr = sk.accept()
-    print("hello client,ip:")
-    print(addr)
-    t = threading.Thread(target=receiveThread,args=(conn,))
-    t.setDaemon(True)
-    t.start()
 
+
+def server():
+    while True:
+        conn, addr = sk.accept()
+        print("hello client,ip:")
+        print(addr)
+        t = threading.Thread(target=receiveThread, args=(conn,))
+        t.setDaemon(True)
+        t.start()
+
+
+tmp = threading.Thread(target=server, args=())
+tmp.setDaemon(True)
+tmp.start()
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
