e0b578ae73c1aba9b926d0bc8d615a98d307648f
QinYUN575 <QinYUN575@Gmail.com>
2020-09-09 01:05:39 +0800

    Modify:Add mic-array IO configuration demo

--- hardware/demo_mic_array.py
+++ hardware/demo_mic_array.py
@@ -3,6 +3,7 @@ import lcd
 
 lcd.init()
 mic.init()
+#mic.init(i2s_d0=23, i2s_d1=22, i2s_d2=21, i2s_d3=20, i2s_ws=19, i2s_sclk=18, sk9822_dat=24, sk9822_clk=25)
 
 while True:
     imga = mic.get_map()
@@ -12,4 +13,3 @@ while True:
     imgc = imgb.to_rainbow(1)
     a = lcd.display(imgc)
 mic.deinit()
-
