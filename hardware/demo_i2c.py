e27a1cd8df47cbb2a73efc5b7bf8721c122a3469
Juwan <junhuanchen@qq.com>
2020-10-10 09:56:14 +0800

    [i2s & spi] add software example.

--- hardware/demo_i2c.py
+++ hardware/demo_i2c.py
@@ -1,6 +1,8 @@
 from machine import I2C
 
-i2c = I2C(I2C.I2C0, freq=100000, scl=28, sda=29)
+# i2c = I2C(I2C.I2C0, freq=100000, scl=28, sda=29) # hardware i2c
+i2c = I2C(I2C.I2C3, freq=100000, scl=28, sda=29) # software i2c
+
 devices = i2c.scan()
 print(devices)
 
---
+++ hardware/demo_spi_soft.py
@@ -0,0 +1,22 @@
+from machine import SPI
+from fpioa_manager import fm
+
+mosi=8
+miso=15
+cs=20
+clk=21
+
+spi = SPI(SPI.SPI_SOFT, mode=SPI.MODE_MASTER, baudrate=400*1000, polarity=0, phase=0, bits=8, firstbit=SPI.MSB, sck=clk, mosi=mosi, miso=miso)
+fm.register(cs, fm.fpioa.GPIO6, force=True)
+cs = GPIO(GPIO.GPIO6, GPIO.OUT)
+
+# read spi flash id
+while True:
+    cs.value(0)
+    write_data = bytearray([0x90, 0x00, 0x00, 0x00])
+    spi.write(write_data)
+    id_buf = bytearray(2)
+    spi.readinto(id_buf, write=0xff)
+    work_data = id_buf
+    cs.value(1)
+    print(work_data)
