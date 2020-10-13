36d81614b7191fa886d06b4954d73abb234d9b38
Juwan <junhuanchen@qq.com>
2020-10-13 12:17:48 +0800

    [ws2812] updated ws2812 example.

--- modules/grove/ws2812/ws2812.py
+++ modules/grove/ws2812/ws2812.py
@@ -1,27 +1,19 @@
-from board import board_info
-##############demo1
 from modules import ws2812
-class_ws2812 = ws2812(board_info.D[4],30)
-for i in range(30):
-    class_ws2812.set_led(i,(0xff,0,0))
-class_ws2812.display()
-from board import board_info
-#############demo2
-from modules import ws2812
-class_ws2812 = ws2812(board_info.D[4],30)
-r=0
-dir = True
+
+# test maix cube grove ws2813 24
+led_io, led_num = 24, 24
+
+ws = ws2812(led_io, led_num)
+
+for i in range(led_num):
+    ws.set_led(i, (0, 0, 0))
+ws.display()
+
+import time, math
+value = 0
 while True:
-    if dir:
-        r += 1
-    else:
-        r -= 1
-    if r>=255:
-        r = 255
-        dir = False
-    elif r<0:
-        r = 0
-        dir = True
-    for i in range(30):
-        a = class_ws2812.set_led(i,(r,0,0))
-    a=class_ws2812.display()
+  value += 1
+  for i in range(value, value + led_num):
+    ws.set_led(i % led_num, (value + i, value + i, value + i))
+  ws.display()
+  time.sleep_ms(10)
