73dcbfb3a5af5ab8a91b19369fd9ff8ac0eb6a22
Juwan <junhuanchen@qq.com>
2020-10-20 19:03:17 +0800

    [speech_recognizer] add isolated_word

---
+++ multimedia/speech_recognizer/isolated_word.py
@@ -0,0 +1,72 @@
+# Copyright (c) sipeed.com
+#
+# Licensed under the MIT license:
+#   http://www.opensource.org/licenses/mit-license.php
+#
+
+import time
+from Maix import GPIO, I2S
+from fpioa_manager import fm
+
+# user setting
+sample_rate   = 16000
+record_time   = 4  #s
+
+#fm.register(8,  fm.fpioa.GPIO0, force=True)
+#wifi_en = GPIO(GPIO.GPIO0, GPIO.OUT)
+#wifi_en.value(0)
+
+fm.register(20,fm.fpioa.I2S0_IN_D0, force=True)
+fm.register(18,fm.fpioa.I2S0_SCLK, force=True)
+fm.register(19,fm.fpioa.I2S0_WS, force=True)
+
+rx = I2S(I2S.DEVICE_0)
+rx.channel_config(rx.CHANNEL_0, rx.RECEIVER, align_mode=I2S.STANDARD_MODE)
+rx.set_sample_rate(sample_rate)
+print(rx)
+
+from speech_recognizer import isolated_word
+
+# model
+sr = isolated_word(dmac=2, i2s=I2S.DEVICE_0, size=10)
+print(sr.size())
+print(sr)
+
+## threshold
+sr.set_threshold(0, 0, 10000)
+
+## record and get & set
+
+while True:
+  time.sleep_ms(100)
+  print(sr.state())
+  if sr.Done == sr.record(0):
+    data = sr.get(0)
+    print(data)
+    break
+  if sr.Speek == sr.state():
+    print('spark A')
+
+sr.set(1, data)
+
+while True:
+  time.sleep_ms(100)
+  print(sr.state())
+  if sr.Done == sr.record(2):
+    data = sr.get(2)
+    print(data)
+    break
+  if sr.Speek == sr.state():
+    print('spark B')
+
+sr.set(3, data)
+
+## recognizer
+print('recognizer')
+while True:
+  time.sleep_ms(200)
+  print(sr.state())
+  print(sr.dts(data))
+  if sr.Done == sr.recognize():
+    res = sr.result()
+    print(res)
