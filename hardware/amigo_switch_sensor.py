30b6a8c7287041a42d06c9ed78f830b1b1d35957
Juwan <junhuanchen@qq.com>
2020-10-23 14:05:42 +0800

    [test] updated amigo_sensor.py

--- hardware/amigo_switch_sensor.py
+++ hardware/amigo_switch_sensor.py
@@ -1,43 +1,11 @@
-# for amigo_switch_sensor
+
+from machine import I2C
+axp173 = I2C(I2C.I2C3, freq=100000, scl=24, sda=27)
+axp173.writeto_mem(0x34, 0x27, 0x20)
+axp173.writeto_mem(0x34, 0x28, 0x0C)
+
 import sensor, image, time, lcd
-
-from machine import I2C, Timer
-import lcd, utime
-from fpioa_manager import fm
-from Maix import GPIO
-import sensor, image, time
-
-class AXP173:
-    class PMUError(Exception):
-        pass
-    class OutOfRange(PMUError):
-        pass
-    def __init__(self, i2c_dev=None, i2c_addr=0x34):
-        from machine import I2C
-        if i2c_dev is None:
-            try:
-                self.i2cDev = I2C(I2C.I2C0, freq=400000, scl=24, sda=27)
-            except Exception:
-                raise PMUError("Unable to init I2C0 as Master")
-        else:
-            self.i2cDev = i2c_dev
-        self.i2cDev.scan()
-        self.axp173Addr = i2c_addr
-    def __write_reg(self, reg_address, value):
-        self.i2cDev.writeto_mem(
-            self.axp173Addr, reg_address, value, mem_size=8)
-    def writeREG(self, regaddr, value):
-        self.__write_reg(regaddr, value)
-
 lcd.init()
-i2cDev = I2C(I2C.I2C0, freq=400000, scl=24, sda=27)
-print(i2cDev.scan())
-
-axp173 = AXP173()
-axp173.writeREG(0x27, 0x20)
-axp173.writeREG(0x28, 0x0C)
-
-lcd.init(freq=20000000)
 
 while True:
 
@@ -64,8 +32,7 @@ while True:
         sensor.set_vflip(1)
         sensor.skip_frames(time=2000)
         for i in range(50):
-            img = sensor.snapshot() # .rotation_corr(x_rotation = 180, z_rotation = +90)
+            img = sensor.snapshot().rotation_corr(z_rotation = +90)
             lcd.display(img)
-
     except Exception as e:
         print(e)
