c2b7b6b00ec417741085f209a93c723654704840
Juwan <junhuanchen@qq.com>
2020-11-12 11:12:05 +0800

    Update demo_camera.py

--- hardware/demo_camera.py
+++ hardware/demo_camera.py
@@ -3,6 +3,8 @@ import sensor, lcd
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
+# sensor.set_hmirror(1) # cube & amigo
+# sensor.set_vflip(1) # cube & amigo
 sensor.run(1)
 sensor.skip_frames()
 lcd.init(freq=15000000)
