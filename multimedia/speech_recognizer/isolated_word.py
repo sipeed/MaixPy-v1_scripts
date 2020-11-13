24bc83cd7ff76965a10df31af1038509dd66363b
Juwan <junhuanchen@qq.com>
2020-11-13 17:42:51 +0800

    Update isolated_word.py

--- multimedia/speech_recognizer/isolated_word.py
+++ multimedia/speech_recognizer/isolated_word.py
@@ -20,6 +20,9 @@ fm.register(20,fm.fpioa.I2S0_IN_D0, force=True)
 fm.register(18,fm.fpioa.I2S0_SCLK, force=True)
 fm.register(19,fm.fpioa.I2S0_WS, force=True)
 
+# fm.register(32,fm.fpioa.I2S0_SCLK, force=True)  # dock
+# fm.register(30,fm.fpioa.I2S0_WS, force=True)    # dock
+
 rx = I2S(I2S.DEVICE_0)
 rx.channel_config(rx.CHANNEL_0, rx.RECEIVER, align_mode=I2S.STANDARD_MODE)
 rx.set_sample_rate(sample_rate)
