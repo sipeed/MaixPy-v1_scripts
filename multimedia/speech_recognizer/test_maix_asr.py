d1d95a4d2fbe4c4b87d683c5fb79fda1fe3f9aae
Juwan <junhuanchen@qq.com>
2020-11-18 10:12:31 +0800

    Update test_maix_asr.py

--- multimedia/speech_recognizer/test_maix_asr.py
+++ multimedia/speech_recognizer/test_maix_asr.py
@@ -11,13 +11,18 @@ record_time   = 4  #s
 #wifi_en = GPIO(GPIO.GPIO0, GPIO.OUT)
 #wifi_en.value(0)
 
+# bit & duino
 fm.register(20,fm.fpioa.I2S0_IN_D0, force=True)
 fm.register(18,fm.fpioa.I2S0_SCLK, force=True)
 fm.register(19,fm.fpioa.I2S0_WS, force=True)
 
+# fm.register(30,fm.fpioa.I2S0_WS, force=True)    # dock
+# fm.register(32,fm.fpioa.I2S0_SCLK, force=True)  # dock
+
+
 rx = I2S(I2S.DEVICE_0)
 rx.channel_config(rx.CHANNEL_0, rx.RECEIVER, align_mode=I2S.STANDARD_MODE)
-#rx.set_sample_rate(sample_rate)
+rx.set_sample_rate(sample_rate)
 print(rx)
 
 from speech_recognizer import asr
