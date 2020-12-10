4dc269b58b7cde83aaff71d7dea9d037fd392fd6
vamoosebbf <38994993+vamoosebbf@users.noreply.github.com>
2020-12-10 15:32:18 +0800

    Update network_wiznet5k.py

--- network/network_wiznet5k.py
+++ network/network_wiznet5k.py
@@ -7,23 +7,24 @@ from fpioa_manager import fm
 server_ip      = "192.168.0.141"
 server_port    = 8000
 
-SPI_SCK = 21
-SPI_MOSI = 8
-SPI_MISO = 15
-SPI_CS = 20
+WIZNET5K_SPI_SCK = 21
+WIZNET5K_SPI_MOSI = 8
+WIZNET5K_SPI_MISO = 15
+WIZNET5K_SPI_CS = 20
 #######################################
 
 addr = (server_ip, server_port)
 spi1 = SPI(4, mode=SPI.MODE_MASTER, baudrate=600 * 1000,
-            polarity=0, phase=0, bits=8, firstbit=SPI.MSB, sck=SPI_SCK, mosi=SPI_MOSI, miso = SPI_MISO)
+            polarity=0, phase=0, bits=8, firstbit=SPI.MSB, sck=WIZNET5K_SPI_SCK, mosi=WIZNET5K_SPI_MOSI, miso = WIZNET5K_SPI_MISO)
 
-nic = network.WIZNET5K(spi = spi1, cs = SPI_CS)
+#  create wiznet5k nic
+nic = network.WIZNET5K(spi = spi1, cs = WIZNET5K_SPI_CS)
 print("Static IP: ", nic.ifconfig())
 
 # #dhcp: Dynamic IP acquisition, It's not necessary
 # while True:
 #     if(nic.dhclient()):
-#         print("DHCP:", nic.ifconfig() )
+#         print("DHCP IP:", nic.ifconfig() )
 #         break;
 
 ############################## TCP Test ##############################
