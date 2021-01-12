7f05e7a3e9393ee14ea6bab8635f179c91c0d852
vamoosebbf <38994993+vamoosebbf@users.noreply.github.com>
2021-01-12 14:51:04 +0800

    Update demo_i2c.py

--- hardware/demo_i2c.py
+++ hardware/demo_i2c.py
@@ -1,8 +1,11 @@
 from machine import I2C
+from fpioa_manager import fm
 
 # i2c = I2C(I2C.I2C0, freq=100000, scl=28, sda=29) # hardware i2c
 i2c = I2C(I2C.I2C3, freq=100000, scl=28, sda=29) # software i2c
-
+#i2c = I2C(I2C.I2C_SOFT, freq=100000, scl=28, sda=29,
+          #gscl = fm.fpioa.GPIOHS1, gsda = fm.fpioa.GPIOHS2) # software i2c for the latest firmware
+          
 devices = i2c.scan()
 print(devices)
 
