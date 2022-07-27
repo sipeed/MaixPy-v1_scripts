7daac8efec1ad63e0fefa44e7970d9e182dd839c
wonderfullook <38576701+wonderfullook@users.noreply.github.com>
2022-07-27 15:13:24 +0800

    Update demo_mic_array.py

--- hardware/demo_mic_array.py
+++ hardware/demo_mic_array.py
@@ -5,6 +5,8 @@ lcd.init()
 mic.init()
 #mic.init(i2s_d0=23, i2s_d1=22, i2s_d2=21, i2s_d3=20, i2s_ws=19, i2s_sclk=18, sk9822_dat=24, sk9822_clk=25)
 
+#mic.init(i2s_d0=20, i2s_d1=21, i2s_d2=15, i2s_d3=8, i2s_ws=7, i2s_sclk=6, sk9822_dat=25, sk9822_clk=24)# for maix cube
+
 while True:
     imga = mic.get_map()
     b = mic.get_dir(imga)
